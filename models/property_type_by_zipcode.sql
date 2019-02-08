18f6b83be234b00903096135d69bb1bd2d77742d
Alex Yu <39963890+alexyyu@users.noreply.github.com>
2019-02-08 09:37:54 -0500

    create model with jinja

---
+++ models/property_type_by_zipcode.sql
@@ -0,0 +1,13 @@
+
+select
+    zipcode,
+    {% for property_type in ['house','apartment','townhouse'] %}
+    -- writing sql using jinja instead of repeating the case when clause
+    sum(case when lower(property_type) = '{{property_type}}' then 1 else 0 end) as {{property_type}}_count
+    {% endfor%}
+
+from source_data.listings
+
+group by 1
+
+limit 100
