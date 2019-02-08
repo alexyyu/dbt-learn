5a744e3f039ed9edeb0476249beed3d881ea51d4
Alex Yu <39963890+alexyyu@users.noreply.github.com>
2019-02-08 10:22:20 -0500

    created packages.yml

--- models/property_type_by_zipcode.sql
+++ models/property_type_by_zipcode.sql
@@ -1,7 +1,11 @@
+{% set property_type = dbt_utils.get_column_value(table='source_data') %}
+
+{{ log(property_type, info=True)}}
 
 select
     zipcode,
     {% for property_type in ['house','apartment','townhouse'] %}
+
     -- writing sql using jinja instead of repeating the case when clause
     sum(case when lower(property_type) = '{{property_type}}' then 1 else 0 end) as {{property_type}}_count
 
